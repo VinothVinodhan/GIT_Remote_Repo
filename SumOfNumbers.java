1c2f582d20d4c0cacf4ede59960d892369459c51
VinothVinodhan <vinothkumar280@gmail.com>
2019-04-15 16:57:55 +0530

    Commit done

---
+++ SumOfNumbers.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class SumOfNumbers {
+// Hi
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 0;
+		while (a <= 10) {
+			b = b + a;
+			a = a + 1;
+
+		}
+		System.out.println(b);
+
+	}
+
+}
